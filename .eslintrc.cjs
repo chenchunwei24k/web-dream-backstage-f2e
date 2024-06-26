8b9ed4ee7c57385dd0979f89ee27046e23552f66
張維倫 <dino.chang@zhangweilundeMacBook-Pro.local>
2024-06-26 22:16:08 +0800

    add eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,8 @@ module.exports = {
   root: true,
   'extends': [
     'plugin:vue/vue3-essential',
-    'eslint:recommended'
+    'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
